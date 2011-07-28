a6136110f2df06aceca17c744366135dd093dbe7
Julien Finet <julien.finet@kitware.com>
2011-07-28 16:20:56 -0400

    ENH: Add models hierarchy in Data module

--- Libs/qMRMLWidgets/qMRMLTreeView.cxx
+++ Libs/qMRMLWidgets/qMRMLTreeView.cxx
@@ -266,6 +266,7 @@ void qMRMLTreeView::setSceneModelType(const QString& modelName)
   if (newFilterModel)
     {
     newFilterModel->setNodeTypes(this->nodeTypes());
+    newFilterModel->setShowHidden(this->showHidden());
     }
   d->setSceneModel(newModel);
   // typically a no op except for ModelHierarchy
